9d2d3bb4817b350ba0d81133dc1fba7c4b4ffc26
nlfox <nlfox@msn.cn>
2016-04-23 15:36:01 +0800

    91

---
+++ 91_Decode_Ways.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def numDecodings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if not s: return 0
+        len_s = len(s)
+        dp = [1] + [0] * len_s
+        for i in xrange(1, len_s + 1):
+            if s[i - 1] != '0':
+                dp[i] += dp[i - 1]
+            if i >= 2 and 10 <= int(s[i - 2: i]) <= 26:
+                dp[i] += dp[i - 2]
+        return dp[len_s]
+
+
+s=Solution()
+print s.numDecodings("12121")
