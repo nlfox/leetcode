7f00f14bffa5bacc72ab833f90ed9ad1cb8ef8ab
nlfox <nlfox@msn.cn>
2016-04-29 22:48:44 +0800

    242

---
+++ 242_Valid_Anagram.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        s = list(s)
+        t = list(t)
+        s.sort()
+        t.sort()
+        print s,t
+        return t == s
+
+
+s = Solution()
+print s.isAnagram("anagram", "nagaram")
