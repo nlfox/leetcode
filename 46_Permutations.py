92b92252a898427780fcca312b08390f334e721e
nlfox <nlfox@msn.cn>
2016-05-07 21:23:20 +0800

    328

---
+++ 46_Permutations.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        self.nums = nums
+        pass
+
+    def dfs(self, l, num):
+        
+        pass
