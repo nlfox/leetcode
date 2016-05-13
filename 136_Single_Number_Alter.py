c5681f403e730963d5ddc08c671fee04a099faec
nlfox <nlfox@msn.cn>
2016-05-13 22:42:17 +0800

    136

---
+++ 136_Single_Number_Alter.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        res = 0
+        for i in nums:
+            res = res ^ i
+        return res
+
+
+print Solution().singleNumber([2,2,4,4,3])
