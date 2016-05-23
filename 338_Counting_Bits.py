7155c06b0037ab0629827651a1bb96172c68e0b6
nlfox <nlfox@msn.cn>
2016-05-23 15:31:30 +0800

    338

---
+++ 338_Counting_Bits.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def countBits(self, num):
+        """
+        :type num: int
+        :rtype: List[int]
+        """
+        res=[]
+        for i in xrange(num):
+            res.append(bin(i)[2:].count('1'))
+        return res
