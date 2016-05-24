15d7df3d7cbecc2d08d45ef3624f7eae7a81159a
nlfox <nlfox@msn.cn>
2016-05-24 22:40:49 +0800

    274

---
+++ 274_H-Index.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def hIndex(self, citations):
+        citations.sort()
+        n = len(citations)
+        for i in xrange(n):
+            if citations[i] >= (n - i):
+                return n - i
+        return 0
+
+
+print Solution().hIndex([3, 0, 6, 1, 5,4,4])
