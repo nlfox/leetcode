4574e913a869ee413f501d709bc81edf75ea3054
nlfox <nlfox@msn.cn>
2016-05-28 22:36:44 +0800

    215

---
+++ 215_Kth_Largest_Element_in_an_Array.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        nums.sort(reverse=True)
+        return nums[k-1]
+
+
+print Solution().findKthLargest([3, 2, 1, 5, 6, 4], 2)
