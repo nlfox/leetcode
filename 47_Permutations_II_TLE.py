717ea0bf4a7cc8a9d3e5609b706b5f3308efb34f
nlfox <nlfox@msn.cn>
2016-06-08 00:11:34 +0800

    47(un)

---
+++ 47_Permutations_II_TLE.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def permuteUnique(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+
+        self.nums = nums
+        self.numLen = len(nums)
+        self.res = []
+        self.l = [True] * self.numLen
+        self.uni = {}
+        self.dfs([])
+        return self.res
+
+    def dfs(self, s):
+        flag = True
+        for i in xrange(self.numLen):
+            if self.l[i]:
+                flag = False
+                self.l[i] = False
+                s += [self.nums[i]]
+                self.dfs(s)
+                s.pop()
+                self.l[i] = True
+        if flag:
+            st = str([i for i in s])
+            if not self.uni.has_key(st):
+                self.uni[st] = True
+                self.res.append([i for i in s])
+print Solution().permuteUnique([1,1,2])
