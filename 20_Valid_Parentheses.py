3aeab5ed5ed5bc187d8b1d14eedeede48065350e
nlfox <nlfox@msn.cn>
2016-06-10 13:59:25 +0800

    20

---
+++ 20_Valid_Parentheses.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+        m = {'{': '}', '[': ']', '(': ')'}
+        for i in xrange(len(s)):
+            if m.has_key(s[i]):
+                stack.append(s[i])
+            else:
+                if stack == []:
+                    return False
+                if m[stack.pop()] == s[i]:
+                    continue
+                else:
+                    return False
+        if stack != []:
+            return False
+        return True
+print Solution().isValid("")
