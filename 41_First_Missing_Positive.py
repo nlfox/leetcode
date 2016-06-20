bd8e9339c1f139a63246fe4441d588e4c32f2fd5
nlfox <nlfox@msn.cn>
2016-06-20 23:02:20 +0800

    41

--- 41_First_Missing_Positive.py
+++ 41_First_Missing_Positive.py
@@ -4,4 +4,17 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-print Solution().firstMissingPositive([3,4,-1,1])
+        nums.append(0)
+        n = len(nums)
+        for i in range(len(nums)):  # delete those useless elements
+            if nums[i] < 0 or nums[i] >= n:
+                nums[i] = 0
+        a = [False for i in xrange(n + 2)]
+        for i in nums:
+            a[i] = True
+        for i in xrange(1, n + 1):
+            if not a[i]:
+                return i
+
+
+print Solution().firstMissingPositive([1])
