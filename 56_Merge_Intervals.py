1e3787c0bd74d3f920298e43449f41ed802b2789
nlfox <nlfox@msn.cn>
2016-07-20 15:59:39 -0400

    56

--- 56_Merge_Intervals.py
+++ 56_Merge_Intervals.py
@@ -1,8 +1,8 @@
-#Definition for an interval.
-class Interval(object):
-    def __init__(self, s=0, e=0):
-        self.start = s
-        self.end = e
+# Definition for an interval.
+# class Interval(object):
+#     def __init__(self, s=0, e=0):
+#         self.start = s
+#         self.end = e
 
 class Solution(object):
     def merge(self, intervals):
@@ -10,7 +10,15 @@ class Solution(object):
         :type intervals: List[Interval]
         :rtype: List[Interval]
         """
-        sorted(intervals,key=lambda x:x.start)
-
-
-
+        intvs = sorted(intervals,key=lambda x:x.start)
+        p = 0
+        res = []
+        l = len(intvs)
+        while p < l-1:
+            if intvs[p].end >= intvs[p+1].start:
+                intvs[p].end = max(intvs[p].end,intvs[p+1].end)
+                intvs.pop(p+1)
+                l -= 1
+            else:
+                p += 1
+        return intvs
