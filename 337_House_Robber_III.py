24dd2e8d4a15016ccac9c6f1f6de449560dfcdf5
nlfox <nlfox@msn.cn>
2016-08-04 23:56:36 -0400

    337

---
+++ 337_House_Robber_III.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def rob(self, root):
+        return self.robDFS(root)[1];
+    def robDFS(self,node):
+        if node is None:
+            return (0,0)
+        l = self.robDFS(node.left)
+        r = self.robDFS(node.right)
+        return (l[1] + r[1], max(l[1] + r[1], l[0] + r[0] + node.val))
